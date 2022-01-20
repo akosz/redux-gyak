97fa6e3c697c8adac4ad7f32ae8e51fcfa5237d1
Akos Szabo <szabo.akos2000@gmail.com>
2022-01-20 14:36:02 +0100

    edit and delete todo

--- src/App.js
+++ src/App.js
@@ -2,30 +2,50 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo } from "./actions";
+import { addTodo, deleteTodo, editTodo } from "./actions";
 import { getTodo } from "./selector";
 
 function App() {
   const [value, setValue] = useState("");
+  const [key, setKey] = useState("");
 
   const dispatch = useDispatch();
 
   const todos = useSelector(getTodo);
 
   const handleClick = ({ target }) => {
-    dispatch(addTodo(value));
+    if (key !== "") {
+      dispatch(editTodo(value, key));
+      setKey("");
+    } else dispatch(addTodo(value));
     setValue("");
+  };
+
+  const handleDelete = (todo) => {
+    dispatch(deleteTodo(todo));
+  };
+
+  const handleEdit = (todo, id) => {
+    setValue(todo);
+    setKey(id);
   };
 
   return (
     <div className="App">
       <input value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick={(e) => handleClick(e)}>Felvétel</button>
+      <button onClick={(e) => handleClick(e)}>
+        {key !== "" ? "Módosít" : "Felvétel"}
+      </button>
       <table>
         <tbody>
-          {todos.map((todo, index) => (
-            <tr key={index}>
-              <td>{todo}</td>
+          {todos.map((todo) => (
+            <tr key={todo.id}>
+              <td onClick={() => handleDelete(todo.id)}>{todo.title}</td>
+              <td>
+                <button onClick={() => handleEdit(todo.title, todo.id)}>
+                  Változtat
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
--- src/todosReduxer.js
+++ src/todosReduxer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ADD_TODO } from "./actions";
+import { ADD_TODO, DELETE_TODO, EDIT_TODO } from "./actions";
 
 const initialState = [];
 export const todosReduxer = (state = initialState, action) => {
@@ -7,9 +7,24 @@ export const todosReduxer = (state = initialState, action) => {
 
   if (type === ADD_TODO) {
     const todos = state;
-    const todo = payload;
+    const todo = { title: payload, id: Date.now().toString() };
 
     return [...todos, todo];
   }
+
+  if (type === DELETE_TODO) {
+    const todos = state;
+    const todoID = payload;
+
+    return todos.filter((todo) => todo.id !== todoID);
+  }
+
+  if (type === EDIT_TODO) {
+    const todos = state;
+    const { todo, id } = payload;
+
+    return todos.map((t) => (t.id === id ? { title: todo, id } : t));
+  }
+
   return state;
 };
